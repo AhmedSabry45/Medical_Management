4fddd6f44ad7a27503ccbe5d6706cd111180ff83
Ahmed sabri <ahmed.sabri19987@gmail.com>
2022-08-17 19:46:49 +0200

    Add project files.

---
+++ medicalManagment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clinic
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("ar-EG");
+            Application.Run(new login());
+            //Application.Run(new frmMain());
+        }
+    }
+}
